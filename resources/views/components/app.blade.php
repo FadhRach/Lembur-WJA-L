466736c1a9f7a57e227fd0b8b62e4899253fc244
Fadhlan <fadhlannur007@gmail.com>
2024-03-01 16:55:09 +0700

    manager front end 1/8

---
+++ resources/views/components/app.blade.php
@@ -0,0 +1,19 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Lembur WJA | Lintasarta</title>
+
+{{-- CSS --}}
+<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+{{-- TAILWIND JS/CSS --}}
+@vite(['resources/css/app.css','resources/js/app.js'])
+
+{{-- ALERT --}}
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+{{-- SCRIPT FLOWBITE --}}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+
+{{-- CHART --}}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js" integrity="sha256-R4pqcOYV8lt7snxMQO/HSbVCFRPMdrhAFMH+vr9giYI=" crossorigin="anonymous"></script>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,28 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="title" content="FlexF">
-    <meta name="description" content="Lembur West Java Area">
-    <meta name="robots" content="index, follow">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta name="language" content="English">
-    <meta name="author" content="Themesberg">
-    <title>Login | Lembur App</title>
-
-    {{-- CSS --}}
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-
-    {{-- TAILWIND JS/CSS --}}
-    @vite(['resources/css/app.css','resources/js/app.js'])
-
-    {{-- ALERT --}}
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
-    {{-- SCRIPT FLOWBITE --}}
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+  <x-app />
 </head>
 <body>
     <section class="bg-blue-100 min-h-screen flex items-center justify-center">
@@ -30,7 +9,7 @@
         <div class="bg-gray-100 flex rounded-2xl shadow-lg max-w-3xl p-5 items-center">
           <!-- form -->
           <div class="md:w-1/2 px-8 md:px-16">
-            <h2 class="font-bold text-blue-800 text-4xl">Lembur WJA.</h2>
+            <h2 class="font-bold text-blue-700 text-4xl">Lembur WJA.</h2>
             <p class="text-xs mt-2 text-blue-400">Aplikasi Lembur Lintasarta West Java Area</p>
 
             <form action="" class="flex flex-col gap-4" method="POST">
@@ -45,7 +24,7 @@
                 <p class="text-xs mt-2 ml-2 text-red-400">{{ $message }}</p>
               @enderror
               </div>
-                <button name="submit" type="submit" class="bg-blue-800 rounded-xl text-white py-2 duration-500 mt-4" value="Login">Login</button>
+                <button name="submit" type="submit" class="bg-blue-600 rounded-xl text-white py-2 duration-500 mt-4" value="Login">Login</button>
             </form>
 
           </div>
