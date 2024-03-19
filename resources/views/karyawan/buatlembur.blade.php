bc5096c8fd2403aa4d95630b56a11c6036e2b83e
Fadhlan <fadhlannur007@gmail.com>
2024-03-19 08:24:55 +0700

    Update terbaru

--- resources/views/karyawan/buatlembur.blade.php
+++ resources/views/karyawan/buatlembur.blade.php
@@ -8,7 +8,7 @@
     <x-nav-aside-karyawan/>
   
     <div class="p-4 sm:ml-64">
-        <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+        <div class="p-4 mt-14">
             <main class="w-full flex-grow">
                 <h1 class="text-3xl text-black pb-6">Buat Lembur</h1>
 
@@ -20,7 +20,7 @@
                                 <div class="col-span-6 lg:col-span-2">
                                     <label for="?" class="text-sm font-medium text-gray-900 block mb-2">Nama Karyawan</label>
                                     <input class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-300 focus:border-blue-300 block w-full p-2.5"
-                                    type="text" name="?" id="?"  placeholder="+628123456789" value="{{ old('?') }}">
+                                    type="text" name="?" id="?"  placeholder="+628123456789" value="{{ auth::user()->name }}">
                                 </div>
                                 <div class="col-span-6 lg:col-span-2">
                                     <label for="?" class="text-sm font-medium text-gray-900 block mb-2">Petugas Engineer</label>
