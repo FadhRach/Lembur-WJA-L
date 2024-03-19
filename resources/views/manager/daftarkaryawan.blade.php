57e554d454bbb880d92f015578cfffeeccdbc6f5
Fadhlan <fadhlannur007@gmail.com>
2024-03-19 11:46:14 +0700

    search cari karyawan coba

--- resources/views/manager/daftarkaryawan.blade.php
+++ resources/views/manager/daftarkaryawan.blade.php
@@ -38,7 +38,7 @@
                             </div>
                             <input type="text" id="table-search-users"
                                 class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                placeholder="Search for users">
+                                placeholder="Cari Karyawan">
                         </div>
                     </div>
                     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
